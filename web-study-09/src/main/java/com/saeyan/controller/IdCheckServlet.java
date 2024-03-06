a6ac6d60144326d080becc85da3c0eb4f6f33795
daechul <rlaeocjf@gmail.com>
2024-03-06 15:45:01 +0900

    test

--- web-study-09/src/main/java/com/saeyan/controller/IdCheckServlet.java
+++ web-study-09/src/main/java/com/saeyan/controller/IdCheckServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.saeyan.dao.MemberDAO;
+
 @WebServlet("/idCheck.do")
 public class IdCheckServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -14,7 +16,13 @@ public class IdCheckServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String userid = request.getParameter("userid");
-		System.out.println("userid >>> " + userid);
+		
+		MemberDAO mDao = MemberDAO.getInstance();
+		
+		int result = mDao.confirmID(userid);  //중복체크메소드 호출
+		
+		System.out.println("중복이니 ?  " +result);
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- web-study-09/src/main/java/com/saeyan/dao/MemberDAO.java
+++ web-study-09/src/main/java/com/saeyan/dao/MemberDAO.java
@@ -2,11 +2,17 @@ package com.saeyan.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 //싱글톤.......
 public class MemberDAO {
 	
 	private static MemberDAO instance = new MemberDAO();
+	
+	private Connection con = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
 	
 	private MemberDAO() {
 	}
@@ -25,4 +31,61 @@ public class MemberDAO {
 		//2. DB연결		
 		return DriverManager.getConnection(url, user, password);
 	}
+
+	//ID중복체크 
+	public int confirmID(String userid) {
+		
+		int result = -1;
+		
+		String sql = "select userid from member where userid = ?";
+		
+		try {
+			
+			//1. 연결
+			con = getConnection();
+			//2. sql문 전송
+			pstmt = con.prepareStatement(sql);
+			//3. 맵핑
+			pstmt.setString(1, userid);
+			//4. 실행 및 결과값 받기
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				result = 1;
+			}else {
+				result  = -1;
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(con != null) con.close();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
